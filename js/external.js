168db16f114dee6feaaf3f1c016e28cc2aab7dc3
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-07-26 14:47:32 -0500

    initial cimmit

---
+++ js/external.js
@@ -0,0 +1,12 @@
+console.log("This is my external file")
+alert("This is my external file");
+
+alert("This is my second alert");
+
+var isStudent= confirm ("Click Ok if Are you a student");
+console.log(isStudent);
+
+var firstname;
+var lastname;
+
+

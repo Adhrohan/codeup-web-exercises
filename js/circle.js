43ed01d24cdb7a2fe915a12cddb7a80b619f06a9
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-08-08 14:51:07 -0500

    math objects

---
+++ js/circle.js
@@ -0,0 +1,19 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max-min + 1) + min)
+}
+console.log(getRandomInt(3, 10));
+
+console.log(Math.round(20.49));
+console.log(Math.round(20.5));
+console.log(Math.round(-20.5));
+console.log(Math.round(-20.51));
+
+
+console.log(Math.floor(45.95));
+console.log(Math.floor(-45.95));
+
+console.log(Math.ceil(.95));
+console.log(Math.ceil(4));
+
+console.log(Math.pow(2, 16));
+console.log(Math.pow(2, 3));

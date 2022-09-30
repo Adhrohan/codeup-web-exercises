094a0b52df3baab8ae56606581f9ef8cd433ea26
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-09-29 22:42:17 -0500

    loops

--- js/loops.js
+++ js/loops.js
@@ -6,3 +6,11 @@ for(num = 5; num <= 100; num += 5){
     }
     console.log("num is still less than 50")
 }
+let number;
+for(number = 1; number <= 100; number += 10){
+    console.log(number);
+    if (number === 50){
+        break;
+    }
+    console.log("num is still less than 50")
+}
